69e9b8e50ef1d50ea6d6d598a2b7e25192f3b436
mirnatul <www.mirnatul@gmail.com>
2025-01-17 02:54:41 +0600

    check github commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">
+      <h1 className="text-3xl font-bold text-center">
         Match Maker
       </h1>
     </>
